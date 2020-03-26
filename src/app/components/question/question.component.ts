ec89e431822fc6c158ea85a1fd43695cad3fa045
duane <duanehaworth@hotmail.com>
2020-03-26 08:08:49 -0500

    Adding click event

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -9,8 +9,24 @@ export class QuestionComponent implements OnInit {
 
   @Input() q: any;
   selectedOption: string;
+  isCorrectAnswer: boolean = false;
 
   constructor() { }
+
+  onSubmitClick(){
+    console.log(this.q);
+    console.log(this.selectedOption);
+
+    const selectedOption = parseInt(this.selectedOption);
+    
+    if(this.q.answers[selectedOption].isCorrect){      
+      this.isCorrectAnswer = true;
+    } else {
+      alert("Incorrect");
+      this.isCorrectAnswer = false;
+    }    
+
+  }
 
   ngOnInit(): void {
   }
