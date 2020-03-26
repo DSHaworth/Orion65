8f08e8fe81bac010ec12626a19982203d14f14eb
duane <duanehaworth@hotmail.com>
2020-03-26 07:50:41 -0500

    Getting Selected index

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class QuestionComponent implements OnInit {
 
   @Input() q: any;
+  selectedOption: string;
 
   constructor() { }
 
